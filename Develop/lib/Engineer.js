9a6a2be0be6aa670f0704712dee5cf18da5898f5
Collin Porter <colls.potta@gmail.com>
2020-09-26 14:50:38 -0600

    changed const name to = file path name

--- Develop/lib/Engineer.js
+++ Develop/lib/Engineer.js
@@ -1,5 +1,5 @@
 // TODO: Write code to define and export the Engineer class.  HINT: This class should inherit from Employee.
-// const Engineer = require("../lib/Employee")
+const employee = require("../lib/Employee")
 
 class Engineer {
     constructor(id, name, email, github) {
