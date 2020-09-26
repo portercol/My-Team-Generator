0019802ce11dc98d374978dcf3e2cecc28c4d33d
Collin Porter <colls.potta@gmail.com>
2020-09-25 18:21:45 -0600

    changed class to const for require

--- Develop/lib/Engineer.js
+++ Develop/lib/Engineer.js
@@ -1,2 +1,2 @@
 // TODO: Write code to define and export the Engineer class.  HINT: This class should inherit from Employee.
-class Engineer = require("../lib/Employee")
+const Engineer = require("../lib/Employee")
