ae51e8eb58cb6d6a36d304c46391126c6ba8b273
Collin Porter <colls.potta@gmail.com>
2020-09-25 18:13:11 -0600

    added Employee class with constructor

--- Develop/lib/Employee.js
+++ Develop/lib/Employee.js
@@ -1,1 +1,25 @@
 // TODO: Write code to define and export the Employee class
+class Employee {
+    constructor(id, name, email) {
+        this.id = id;
+        this.name = name;
+        this.email = email;
+    }
+
+    getId() {
+        return this.id
+    };
+
+    getName() {
+        return this.name
+    };
+
+    getEmail() {
+        return this.email
+    };
+
+    getPosition() {
+        return this.position
+    };
+};
+module.exports = Employee;
