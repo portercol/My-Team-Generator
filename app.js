21322de9e4231c83acbb66c755d93980ffea4b21
Collin Porter <colls.potta@gmail.com>
2020-09-26 21:08:52 -0600

    added prompt questions and promise function

--- app.js
+++ app.js
@@ -14,6 +14,31 @@ const render = require("./lib/htmlRenderer");
 // Write code to use inquirer to gather information about the development team members,
 // and to create objects for each team member (using the correct classes as blueprints!)
 
+inquirer.prompt([
+    {
+        type: "input",
+        name: "name",
+        message: "What is your name?"
+    },
+    {
+        type: "input",
+        name: "id",
+        message: "What is your ID number?"
+    },
+    {
+        type: "input",
+        name: "email",
+        message: "What is your email?"
+    },
+    {
+        type: "input",
+        name: "role",
+        message: "What is your role?"
+    }
+])
+.then (function(response){
+    return response
+});
 // After the user has input all employees desired, call the `render` function (required
 // above) and pass in an array containing all employee objects; the `render` function will
 // generate and return a block of HTML including templated divs for each employee!
