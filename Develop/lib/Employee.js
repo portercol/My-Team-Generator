636c7da11695f21444c170e9803aa8f4118a99b6
Collin Porter <colls.potta@gmail.com>
2020-09-26 21:08:28 -0600

    added role to constructor

--- Develop/lib/Employee.js
+++ Develop/lib/Employee.js
@@ -1,9 +1,10 @@
 // TODO: Write code to define and export the Employee class
 class Employee {
-    constructor(name, id, email) {
+    constructor(name, id, email, role) {
         this.name = name;
         this.id = id;
         this.email = email;
+        this.role = role;
     }
 
     getName() {
