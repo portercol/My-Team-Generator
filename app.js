963d89214d3315c661e6f6b61d123d60371869af
Collin Porter <colls.potta@gmail.com>
2020-09-28 21:07:12 -0600

    removed new instance and edited starter code

--- app.js
+++ app.js
@@ -4,10 +4,8 @@ const Intern = require("./Develop/lib/Intern");
 const inquirer = require("inquirer");
 const path = require("path");
 const fs = require("fs");
-
 const OUTPUT_DIR = path.resolve(__dirname, "output");
 const outputPath = path.join(OUTPUT_DIR, "team.html");
-
 const render = require("./Develop/lib/htmlRenderer");
 const employees = [];
 
