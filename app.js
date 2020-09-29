050d931b63f025ed7ccf79a26647b6e7ec29f5ae
Collin Porter <colls.potta@gmail.com>
2020-09-28 21:08:39 -0600

    changed var name from employees to allEmployees

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ const fs = require("fs");
 const OUTPUT_DIR = path.resolve(__dirname, "output");
 const outputPath = path.join(OUTPUT_DIR, "team.html");
 const render = require("./Develop/lib/htmlRenderer");
-const employees = [];
+const allEmployees = [];
 
 // Write code to use inquirer to gather information about the development team members,
 // and to create objects for each team member (using the correct classes as blueprints!)
