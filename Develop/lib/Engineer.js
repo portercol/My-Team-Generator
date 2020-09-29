6175d6fe15f6762fa459323077885098a558267c
Collin Porter <colls.potta@gmail.com>
2020-09-28 18:55:49 -0600

    added extend to class and super method

--- Develop/lib/Engineer.js
+++ Develop/lib/Engineer.js
@@ -1,11 +1,10 @@
+const Employee = require("../lib/Employee");
 // TODO: Write code to define and export the Engineer class.  HINT: This class should inherit from Employee.
 const employee = require("../lib/Employee")
 
-class Engineer {
+class Engineer extends Employee{
     constructor(id, name, email, github) {
-        this.id = id;
-        this.name = name;
-        this.email = email;
+        super(name, id, email)
         this.github = github;
     }
 
